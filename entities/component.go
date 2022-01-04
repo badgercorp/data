3f71a9c0e9caadf5834e4f69276aaa251717d6cb
Michael Edenzon <michaeledenzon@gmail.com>
2022-01-03 20:12:26 -0500

    added json tags

--- entities/component.go
+++ entities/component.go
@@ -1,14 +1,14 @@
 package entities
 
 type DatabaseComponent struct {
-	ID                     int
-	Component              string
-	Organization           string
-	RepositoryID           string
-	RepositoryName         string
-	RepositoryOrganization string
-	RepositorySlug         string
-	OnboardedAt            string
-	ArchivedAt             string
-	Status                 string
+	ID                     int    `json:"id"`
+	Component              string `json:"component"`
+	Organization           string `json:"organization"`
+	RepositoryID           string `json:"repository_id "`
+	RepositoryName         string `json:"repository_name"`
+	RepositoryOrganization string `json:"repository_owner"`
+	RepositorySlug         string `json:"repository_slug"`
+	OnboardedAt            string `json:"onboarded_at"`
+	ArchivedAt             string `json:"archived_at,omitempty"`
+	Status                 string `json:"status"`
 }
